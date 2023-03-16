17b8f1116f61a0d6eed33694cd45043c1fe61f21
jserra99 <progamer5051@gmail.com>
2023-03-15 19:53:11 -0700

    switching computers

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import com.pathplanner.lib.server.PathPlannerServer;
+
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
@@ -16,6 +18,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    // PathPlannerServer.startServer(5811);
   }
 
   @Override
